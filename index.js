2e5f8dbcc1c150a4767a2bb4ffa99df82744b7a0
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-25 23:06:51 +0600

    client added

--- index.js
+++ index.js
@@ -12,15 +12,26 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l8lkz.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("manufacturer running")
-  // perform actions on the collection object
-  client.close();
-});
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izdcq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        await client.connect();
+        console.log('Database connected');
+    }
+    finally{
+
+    }
+
+
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Manufacturer server');
