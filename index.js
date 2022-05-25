2cc89e95f1ce9f0edccef25f890d7b6d34bd56dd
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-25 20:59:37 +0600

    basic setup

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l8lkz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("manufacturer running")
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Running Manufacturer server');
