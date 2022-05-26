2c6d36d65d446389a878799770e7edb866118083
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-26 21:06:48 +0600

    make user api add

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function run (){
     try{
         await client.connect();
         const partCollection = client.db('manu_factureer').collection('parts');
+        const userCollection = client.db('manu_factureer').collection('users');
 
         app.get('/part', async(req,res)=>{
             const query={};
@@ -31,14 +32,26 @@ async function run (){
             res.send(parts);
         });
 
-        app.post('/add-part', async(req,res)=>{
+        app.put('/user/:email', async(req,res)=>{
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = {upsert: true };
+            const updateDoc = { 
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc,options);
+            res.send(result);
+        })
+
+        app.post('/add-profile', async(req,res)=>{
          
                 const data = req.body;
                 const result = await partCollection.insertOne(data);
                 res.send(result);
            });
 
-           app.put('/update-part/:id', async(req,res)=>{
+           app.put('/update-profile/:id', async(req,res)=>{
                const {id} = req.params;
                const data = req.body;
 
@@ -48,16 +61,40 @@ async function run (){
 
                const result = await partCollection.updateOne(filter, updateDoc,option);
                res.send(result);
-           })
+           });
     }
     finally{
 
     }
-
-
 }
 
 run().catch(console.dir);
+
+// Body
+
+app.get("/dummy-route/user2", async (req, res) => {
+    const data = req.body;
+  
+    res.json(data);
+  });
+
+  // Query
+
+app.get("/dummy-route/user", async (req, res) => {
+    const { education,city,phone } = req.query;
+    console.log(education);
+    console.log(city);
+    res.json(phone);
+  });
+
+  // Param
+
+app.get("/dummy-route/user/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    res.json(id);
+  });
+  
 
 
 app.get('/', async (req, res) => {
