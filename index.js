7bc427b1825096fa2cf4b92adfd45dbd3836ece4
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-27 23:34:30 +0600

    todays last commit

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ async function run (){
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc,options);
-            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: 'h'})
-            res.send({result,token});
+            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '6h'})
+            res.send({result,  token});
         })
 
         // app.post('/add-profile', async(req,res)=>{
