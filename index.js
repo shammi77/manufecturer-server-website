f5807634b5770a00c002612343c2e687fd0a816d
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-27 19:30:44 +0600

    post method added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -41,7 +42,8 @@ async function run (){
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc,options);
-            res.send(result);
+            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: 'h'})
+            res.send({result,token});
         })
 
         // app.post('/add-profile', async(req,res)=>{
