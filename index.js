8df68b1a7db6d20a4740fd8d307d5c8140083e5a
shammi77 <nusratshammi.vutu@gmail.com>
2022-06-03 08:06:49 +0600

    admin added

--- index.js
+++ index.js
@@ -1,89 +1,129 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izdcq.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run (){
-    try{
-        await client.connect();
-        const partCollection = client.db('manu_factureer').collection('parts');
-        const userCollection = client.db('manu_factureer').collection('users');
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+async function run() {
+  try {
+    await client.connect();
+    const partCollection = client.db("manu_factureer").collection("parts");
+    const bookingCollection = client
+      .db("manu_factureer")
+      .collection("bookings");
+    const userCollection = client.db("manu_factureer").collection("users");
 
-        app.get('/part', async(req,res)=>{
-            const query={};
-            const cursor = partCollection.find(query);
-            const parts = await cursor.toArray();
-            res.send(parts);
-        });
+    app.get('/part', async (req, res) => {
+      const query = {};
+      const cursor = partCollection.find(query);
+      const parts = await cursor.toArray();
+      res.send(parts);
+    });
 
-      app.get('/user' , async(req, res)=>{
-          const users = await userCollection.find().toArray();
-          res.send(users);
-      });
-      
-      app.put('/user/admin/:email', async (req, res) => {
+    app.get('/user', async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    // for booking
+
+    app.get('/booking',verifyJWT,  async (req, res) => {
+      const customer = req.query.customer;
+      const decodedEmail = req.decoded.email;
+      if (customer === decodedEmail) {
+        const query = { customer: customer };
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings);
+      }
+       else {
+        return res.status(403).send({ message: "forbidden access " });
+      }
+    });
+
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin});
+    });
+
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
         const email = req.params.email;
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({ email: requester });
+        if(requesterAccount.role === 'admin'){
         const filter = { email: email };
         const updateDoc = {
-          $set:{role : 'admin'},
+          $set: { role: 'admin' },
         };
         const result = await userCollection.updateOne(filter, updateDoc);
-        res.send( result );
-      });
-
-        app.put('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updateDoc = {
-              $set: user,
-            };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' })
-            res.send({ result, token });
-          })
-      
-
-        // app.post('/add-profile', async(req,res)=>{
-         
-        //         const data = req.body;
-        //         const result = await partCollection.insertOne(data);
-        //         res.send(result);
-        //    });
-
-        //    app.put('/update-profile/:id', async(req,res)=>{
-        //        const {id} = req.params;
-        //        const data = req.body;
-
-        //        const filter = { _id: ObjectId(id) };
-        //        const updateDoc = {$set: data};
-        //        const option = {upsert: true};
-
-        //        const result = await partCollection.updateOne(filter, updateDoc,option);
-        //        res.send(result);
-        //    });
-    }
-    finally{
-
-    }
+        res.send(result);
+      }
+      else{
+        res.status(403).send({ message : 'forbidden'});
+      }
+    
+    });
+    app.put("/user/:email",  async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10h" }
+      );
+      res.send({ result, token });
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
@@ -92,7 +132,7 @@ run().catch(console.dir);
 
 // app.get("/dummy-route/user2", async (req, res) => {
 //     const data = req.body;
-  
+
 //     res.json(data);
 //   });
 
@@ -109,16 +149,14 @@ run().catch(console.dir);
 
 // app.get("/dummy-route/user/:id", async (req, res) => {
 //     const { id } = req.params;
-  
+
 //     res.json(id);
 //   });
-  
 
-
-app.get('/', async (req, res) => {
-    res.json('Running Manufacturer server');
+app.get("/", async (req, res) => {
+  res.json("Running Manufacturer server");
 });
 
-app.listen(port, ()=>{
-    console.log('Listening to port',port);
-})
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
